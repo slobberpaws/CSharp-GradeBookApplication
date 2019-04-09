6c069ed4564cc214481caaa00e658d7667321571
slobberpaws <30983358+slobberpaws@users.noreply.github.com>
2019-04-09 15:30:08 -0700

    Steps 2- 6

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,11 @@
+using GradeBook.Enums;
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
