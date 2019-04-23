a5df6bc599fe2791d4a334decd0aab39f14fe529
slobberpaws <30983358+slobberpaws@users.noreply.github.com>
2019-04-23 16:52:40 -0700

    step 1 create IsWeighted Property

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,6 +15,8 @@ namespace GradeBook.GradeBooks
         public List<Student> Students { get; set; }
         public GradeBookType Type {get;set; }
 
+        public bool IsWeighted;
+
         public BaseGradeBook(string name)
         {
             Name = name;
